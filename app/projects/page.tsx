6052f8e33ab0b73882d340c1b007626e83466769
Ali Hussnain <ali.hussnain112003@gmail.com>
2023-03-22 22:53:06 +0500

    d

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,7 @@
-'use client'
 import { FromBottomReveal } from "@/shared_utils/RevealComponents"
 import { FaGithub, FaLinkedinIn, FaSkype, FaTwitter } from "react-icons/fa"
-import { getProjects } from "../../app/cms/projects"
-import { ProjectItem } from "../../app/components/ProjectItem"
+import { getProjects } from "../cms/projects"
+import { ProjectItem } from "../components/ProjectItem"
 
 
 
